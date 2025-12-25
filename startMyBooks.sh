9616ca833948c1b254359c81f2e0c3147ca90821
Eric Vanhove <eric.vanhove@gmail.com>
2025-12-25 15:39:06 -0500

    Start script because I keep forgetting how

---
+++ startMyBooks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ "$VIRTUAL_ENV" !=  "(mybooks)" ]]; then
+  echo "Please activate the virtual environment"
+  echo "source .venv/Scripts/activate"
+  exit 1
+fi
+
+uv run uvicorn main:app --reload
