3f1f207a2e571939d96912807b120d8c9f5bd12f
Eric <eric.vanhove@gmail.com>
2025-12-28 11:55:37 -0500

    working, maybe?

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import text
 
-DATABASE_URL = "sqlite+aiosqlite:///./books.db"
+DATABASE_URL = "sqlite+aiosqlite:///Z:/books.db"
 engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
