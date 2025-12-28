8e5736e5c6c5a644cbf7c9bae9dffedb96923295
Eric Vanhove <eric.vanhove@gmail.com>
2025-12-28 10:37:52 -0500

    Making things work

--- startMyBooks.sh
+++ startMyBooks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -ueo pipefail
+#set -ueo pipefail
 
-if [[ "$VIRTUAL_ENV" !=  "(mybooks)" ]]; then
-  echo "Please activate the virtual environment"
-  echo "source .venv/Scripts/activate"
-  exit 1
-fi
+#if [[ "$VIRTUAL_ENV" !=  "(mybooks)" ]]; then
+#  echo "Please activate the virtual environment"
+#  echo "source .venv/Scripts/activate"
+#  exit 1
+#fi
 
 uv run uvicorn main:app --reload
